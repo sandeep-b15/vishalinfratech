8db2dd0cdbc36dd2e1a68e2a36a6150d5d31fcb5
sandeep-b15 <37730746+sandeep-b15@users.noreply.github.com>
2022-07-05 11:47:16 +0530

    email change

--- forms/quote.php
+++ forms/quote.php
@@ -24,14 +24,14 @@
   $contact->subject = 'Request for a quote';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'gmail.com',
+    'username' => 'y2jsandyx',
+    'password' => '$@NdeeP1994',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
